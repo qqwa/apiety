10b15b9b4aab1103311c386a74d2885f438a9876
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2019-10-06 23:06:08 +0200

    Modify main loop to recveive KeyPairs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <readerwriterqueue.h>
 #include "CaptureStreams.h"
+#include "ReadMem.h"
 
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/bin_to_hex.h>
 #include <argh.h>
-
 
 int main(int, char* argv[]) {
     argh::parser cmdl(argv);
@@ -20,30 +20,36 @@ int main(int, char* argv[]) {
     CaptureStreams captureStreams(queue, remove_queue);
     auto capture_thread = captureStreams.start_thread();
 
+    moodycamel::BlockingReaderWriterQueue<KeyPair> key_queue(10);
+    ReadMem mem(key_queue);
+
     CapturedPacket item;
+    KeyPair key_item;
     StreamIdentifier remove_identifer;
     while(true) {
-        queue.wait_dequeue(item);
+        if (queue.wait_dequeue_timed(item, std::chrono::milliseconds(10))) {
+            auto len = item.payload.size();
+            auto total = 0;
+            switch (item.direction) {
+                case Direction::FromLoginServer:
+                case Direction::FromGameServer:
+                    total = item.identifier.bytes_recv;
+                    break;
+                case Direction::ToLoginServer:
+                case Direction::ToGameServer:
+                    total = item.identifier.bytes_send;
+                    break;
+            }
+            spdlog::info("{} {}({:4d} - {:6d}): {:n}", item.identifier, item.direction, len, total, spdlog::to_hex(std::begin(item.payload), std::begin(item.payload) + std::min<size_t>(len, 10ul)));
+            item.payload.clear();
+        }
+
+        while (key_queue.try_dequeue(key_item)) {
+            // TODO: add KeyPair to keystore
+        }
 
         while (remove_queue.try_dequeue(remove_identifer)) {
             spdlog::info("Removed stream: id:{} server:{}:{}", remove_identifer, remove_identifer.ip.to_string(), remove_identifer.port);
         }
-
-        auto len = item.payload.size();
-        auto total = 0;
-        switch (item.direction) {
-            case Direction::FromLoginServer:
-            case Direction::FromGameServer:
-                total = item.identifier.bytes_recv;
-                break;
-            case Direction::ToLoginServer:
-            case Direction::ToGameServer:
-                total = item.identifier.bytes_send;
-                break;
-        }
-
-        spdlog::debug("{} {}({:4d} - {:6d}): {:n}", item.identifier, item.direction, len, total, spdlog::to_hex(std::begin(item.payload), std::begin(item.payload) + std::min<size_t>(len, 10ul)));
-
-        item.payload.clear();
     }
 }
